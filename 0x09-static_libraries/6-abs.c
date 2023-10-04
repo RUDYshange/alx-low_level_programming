d7be5111d28af51c013f10c4c31e6a07e09d0fa2
RUDYshange <dreadynerdy@gmail.com>
2023-10-03 22:56:12 -0700

    0x09

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the  value of an integer
+ * @n: check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
